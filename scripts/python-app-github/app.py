997f27e5ed96b5e577e933449c71b68752bb4705
Jon <31189528+statzenthusiast921@users.noreply.github.com>
2024-10-06 16:22:28 -0700

    Update app.py

--- scripts/python-app-github/app.py
+++ scripts/python-app-github/app.py
@@ -590,3 +590,7 @@ def update_table_style(selected_year):
 
 if __name__=='__main__':
 	app.run_server()
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))  # Use PORT env var or default to 5000
+    app.run(host="0.0.0.0", port=port)
